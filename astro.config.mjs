4712775abb80e0693f654e5a73ef5ed0ecf63a95
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2025-03-25 22:38:38 +0800

    Set up TailwindCSS integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   server: {
     port: 3000,
+  },
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,json}",
+    "./astro.config.mjs",
+  ],
+  // Allow both automatic and manual dark mode. See:
+  // https://tailwindcss.com/docs/dark-mode#using-multiple-selectors
+  darkMode: [
+    "variant",
+    [
+      "@media (prefers-color-scheme: dark) { &:not(.light *) }",
+      "&:is(.dark *)",
+    ],
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        displaySerif: ["Old Standard TT", ...defaultTheme.fontFamily.serif],
+        serif: ["PT Serif", ...defaultTheme.fontFamily.serif],
+        mono: ["Geist Mono", ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  plugins: [],
+};
